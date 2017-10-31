e7682ad4319ac97dafb9e0a9fc6510a45f244aea
Pablo Pesce <papesce@gmail.com>
2017-10-31 14:36:46 -0300

    example4

--- app2/src/App.js
+++ app2/src/App.js
@@ -2,24 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const InputForm = React.createElement(
-  "form",
-  { target: "_blank", action: "https://google.com/search" },
-  React.createElement("div", null, "Enter input and click Search"),
-  React.createElement("input", { name: "q", className: "input" }),
-  React.createElement(Button, { title: "Search" })
-);
+function InputForm () {
+  return (
+    <form target="_blank" action="https://google.com/search">
+      <div>Enter input and click search</div>
+      <input name="q" className="input"/>
+      <Button title="Search2"/> 
+    </form>
+  )
+} 
+
+const RandomValue = ()  => <div>{Math.floor(Math.random() * 100)} </div>
+
+//const InputForm = React.createElement(
+//  "form",
+//  { target: "_blank", action: "https://google.com/search" },
+//  React.createElement("div", null, "Enter input and click Search"),
+//  React.createElement("input", { name: "q", className: "input" }),
+//  React.createElement(Button, { title: "Search" })
+//);
 
 function Button (props) {
+  let {title} = props;
   return (<div>
-   <input type="text" name="text"/>  
   <button type="submit">{props.title}</button>
   </div>)
 }
 
 class App extends Component {
   render() {
-    return InputForm;
+    //return InputForm;
     return (
       <div className="App">
         <header className="App-header">
@@ -29,8 +41,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button title={"Title"}/>
-        
+        <InputForm/>
+        <RandomValue/>
         
       </div>
     );
