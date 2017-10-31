6e021275e74de83439a0e7efd25aa045f6693aa7
Pablo Pesce <papesce@gmail.com>
2017-10-31 15:40:15 -0300

    example11

--- app2/src/App.js
+++ app2/src/App.js
@@ -2,43 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function InputForm () {
-  return (
-    <form target="_blank" action="https://google.com/search">
-      <div>Enter input and click search</div>
-      <input name="q" className="input"/>
-      <Button title="Search2"/> 
-    </form>
-  )
-} 
 
-const RandomValue = ()  => <div>{Math.floor(Math.random() * 100)} </div>
 
-//const InputForm = React.createElement(
-//  "form",
-//  { target: "_blank", action: "https://google.com/search" },
-//  React.createElement("div", null, "Enter input and click Search"),
-//  React.createElement("input", { name: "q", className: "input" }),
-//  React.createElement(Button, { title: "Search" })
-//);
-
-const MaybeError = ({errorMessage}) => <div>
-    {errorMessage && <ErrorDisplay message={errorMessage}/>}
-  </div>; 
-
-const ErrorDisplay = ({message}) => <div
- style={{color:"red", background:"yellow"}}
->{message}</div>
-
-function Button (props) {
-  let {title} = props;
-  return (<div>
-  <button type="submit">{props.title}</button>
-  </div>)
+class Button extends React.Component {
+  constructor(props){
+    super(props);
+    this.counter = 0;
+  }
+  handleClick = () => {
+    this.counter++;
+    console.log(`Clicked ${this.counter}`)
+  } 
+  render() {
+    return (<div>
+      <button onClick={this.handleClick}>{this.props.title + this.counter}</button>
+      </div>)
+  }
 }
 
-const Doubler = ({value = [1,2,3]}) => 
-  <div>{value.map(e => e * 2 + ",")}</div>;
+//function Button (props) {
+  // let {title} = props;
+  // return (<div>
+  // <button type="submit">{props.title}</button>
+  // </div>)
+// }
+
+
 
 class App extends Component {
   render() {
@@ -52,12 +41,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <InputForm/>
-        <RandomValue/>
-        {/*<ErrorDisplay message="error message"/>*/}
-        <MaybeError errorMessage={Math.random() > 0.5 ? "Random error" : ""}/>
-        <Doubler value={[2]}/>
-      </div>
+        <Button title="Button title "/>
+        </div>
     );
   }
 }
