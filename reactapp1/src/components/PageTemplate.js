76b82d5a91e760e78329bed98a79564f22cd08c2
Pablo Pesce <papesce@gmail.com>
2017-11-07 20:40:13 -0300

    initial commit remove all

--- reactapp1/src/components/PageTemplate.js
+++
@@ -1,9 +0,0 @@
-import {MainMenu} from './menus'
-import React from 'react'
-import './pages.css'
-
-export const PageTemplate = ({children}) =>
-  <div className = "page">
-    <MainMenu />
-    {children}
-  </div>
