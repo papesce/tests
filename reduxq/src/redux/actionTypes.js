b2cd4b6996851242bb6596eea98b25bb0cde6544
Pablo Pesce <papesce@gmail.com>
2017-11-12 18:31:02 -0300

    redux starter

---
+++ reduxq/src/redux/actionTypes.js
@@ -0,0 +1,20 @@
+
+const ADD_TODO = 'ADD_TODO';
+//  {
+//    type: ADD_TODO,
+//    text: "build my first redux app"
+//  }
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+//  {
+//    type: TOGGLE_TODO,
+//    index: 5
+//  }
+const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+// {
+//   type: SET_VISIBILITY_FILTER
+//   filter: SHOW_COMPLETED
+// }    
+
+export {ADD_TODO, REMOVE_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER};
+
