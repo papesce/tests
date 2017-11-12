dbc301700305b79772793f74a6168ceb8be0418e
Pablo Pesce <papesce@gmail.com>
2017-11-12 19:50:20 -0300

    preSplit2

--- reduxq/src/redux/reducers.js
+++ reduxq/src/redux/reducers.js
@@ -17,28 +17,42 @@ const initialState = {
 //}
 //}
 
-function todoAPP (prevState = initialState, action) {
+function todos(prevState = [] , action) {
     switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-        return {...prevState, visibilityFilter: action.filter };
     case ADD_TODO:
-        return {...prevState, todos: [
-            ...prevState.todos,
+        return [...prevState,
             { text: action.text,
                 completed: false
             }
         ]
-        }
     case TOGGLE_TODO:
-        return {...prevState, todos: 
-        prevState.todos.map((todo, index) => {
+        return prevState.map((todo, index) => {
             if (index === action.index) {
                 return Object.assign({}, todo, {completed: !todo.completed})
             } else {
                 return todo
             } 
         })
-        }
+    default: return prevState;     
+    }
+}
+
+function visibilityFilter(prevState = VisibilityFilters.SHOW_ALL, action) {
+    switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+        return action.filter;
+    default: return prevState;     
+    }
+}
+
+function todoAPP (prevState = initialState, action) {
+    switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+        return {...prevState, visibilityFilter: 
+            visibilityFilter(prevState.filter,action) };
+    case ADD_TODO:
+    case TOGGLE_TODO:
+        return {...prevState, todos: todos(prevState.todos, action)}
     default: return prevState;     
     }
 }
