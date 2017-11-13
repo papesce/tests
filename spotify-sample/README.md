3a54fc6bf68bc6bc3c2140ea06d398788beb2d29
Pablo Pesce <papesce@gmail.com>
2017-11-13 13:27:29 -0300

    login using material-ui

--- spotify-sample/README.md
+++ spotify-sample/README.md
@@ -9,3 +9,10 @@ npm install --save react-redux
 npm install --save react-router-redux@next
 npm install --save history
 npm install --save redux-logger
+npm --save install material-ui
+
+
+
+
+#Links
+https://medium.com/technoetics/create-basic-login-forms-using-create-react-app-module-in-reactjs-511b9790dede
--- spotify-sample/src/components/Login.js
+++ spotify-sample/src/components/Login.js
@@ -1,7 +1,59 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 import React from 'react'
 
 
-const Login = () => (<h2> Login </h2>)
+//const Login = () => (<h2> Login </h2>)
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {username: 'pablo', password: ''}
+    this.usernameChanged = this.usernameChanged.bind(this);
+    this.passwordChanged = this.passwordChanged.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  usernameChanged(event, newValue) {
+    this.setState({username: newValue})
+  }
+  passwordChanged(event, newValue ) {
+    this.setState({password: newValue})
+  }
+  handleClick(event) {
+
+  }
+  submitForm() {
+
+  }
+  render() {
+    const email = this.state.username;
+    const password = this.state.password;
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+            <AppBar title="Spotify Login"/>
+            <TextField hintText="Enter your spotify username"
+              floatingLabelText="Username"
+              onChange = {this.usernameChanged}/>
+            <br/>
+            <TextField hintText="Enter your spotify passowrd"
+              type = "password"
+              floatingLabelText="Password"
+              style={{margin: 15}}
+              onChange = {this.passwordChanged}
+            />
+            <br/>
+            <RaisedButton label="Submit" primary={true}
+              onClick={this.handleClick}/>
+          </div>
+        </MuiThemeProvider>
+      </div>
+    )
+  }
+}
 
 
 
