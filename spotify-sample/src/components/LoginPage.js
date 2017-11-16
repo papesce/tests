ce34aaf73e7e9980d84fce445d505616e5b16103
Pablo Pesce <papesce@gmail.com>
2017-11-15 22:05:45 -0300

    change in login page

--- spotify-sample/src/components/LoginPage.js
+++ spotify-sample/src/components/LoginPage.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {withRouter} from 'react-router-dom'
+import Spotify from 'spotify-web-api-js'
 
-const LoginPage = withRouter(
-  ({history}) => (<a href="http://localhost:3001/login"> Login </a>))
+const spotifyApi = new Spotify();
 
+//const LoginPage = withRouter(
+//  ({history}) => (<a href="http://localhost:3001/login"> Login </a>))
+
+class LoginPage extends Component {
+
+  render () {
+    return (<a href="http://localhost:3001/login"> Login </a> )
+    
+  }
+
+}
 
 export default LoginPage
